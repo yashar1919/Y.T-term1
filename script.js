b2880b32f853c1d8e55b6bba32d69d7bb409fc01
Yashar_Talebi <yashar1919>
2021-12-10 19:40:37 +0330

    init

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function darkmode(){
+function darkmode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
-  }
+}
+
+$(document).ready(function () {
+
+  $(".image").mouseenter(function () {
+    $(".image").animate({
+      height: "+=10px",
+      width: "+=10px"
+    });
+  });
+
+
+  $(".image").mouseout(function () {
+    $(".image").animate({
+      height: "-=10px",
+      width: "-=10px"
+    });
+  });
+
+
+});
